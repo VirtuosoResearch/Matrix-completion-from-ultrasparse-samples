f24334a261ed0078a81c43d888cdeb9a33f5d171
hongyangsg <hongyang90@gmail.com>
2023-12-23 21:18:23 -0500

    wip

--- src/gradient_descent.py
+++ src/gradient_descent.py
@@ -1,2 +1,10 @@
-# back up
+import numpy as np
+import random
 
+def gradient_descent_one_step(M, eta):
+	updated_M = np.zeros()
+	return updated_M
+
+def gradient_desent(M, eta, num_of_epochs):
+	updated_M = []
+	return
