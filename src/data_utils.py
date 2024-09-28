f5bafadfbf53ff30ca65cec4e171fb6d1dd3c1ad
zhenshuozhang <ssshore@outlook.com>
2024-09-28 14:02:05 -0400

    datasets

--- src/data_utils.py
+++ src/data_utils.py
@@ -5,6 +5,8 @@ import numpy as np
 from collections import Counter
 import cyvcf2
 from tqdm import tqdm
+import h5py
+import scipy.sparse as sp
 
 import os
 import pickle
@@ -62,6 +64,44 @@ def load_data_all(dataset, s=200):
     elif dataset == 'gene':
         data = np.load(vcf_path)
         return torch.tensor(data, dtype=float)
+    
+    elif dataset == 'sweet':
+        # read table
+        ratings_title = ['product','user', 'value']
+        df = pd.read_csv('./data/sweet.csv')
+        product_ids = {product: idx for idx, product in enumerate(df['product'].unique())}
+        df['product_id'] = df['product'].map(product_ids)
+
+        # 使用pivot_table生成用户-产品评分矩阵
+        rating_matrix = df.pivot_table(index='user', columns='product_id', values='value', fill_value=0)
+
+        # 将DataFrame转换为numpy矩阵
+        matrix_np = rating_matrix.values
+        matrix = torch.tensor(matrix_np)
+
+        return matrix
+    
+    elif dataset in ['douban', 'flixster', 'yahoo_music']:
+        path_file = f'./data/{dataset}.mat'
+        db = h5py.File(path_file, 'r')
+        ds = db['M']
+        try:
+            if 'ir' in ds.keys():
+                data = np.asarray(ds['data'])
+                ir = np.asarray(ds['ir'])
+                jc = np.asarray(ds['jc'])
+                out = sp.csc_matrix((data, ir, jc)).astype(np.float32)
+        except AttributeError:
+            # Transpose in case is a dense matrix because of the row- vs column- major ordering between python and matlab
+            out = np.asarray(ds).astype(np.float32).T
+
+        db.close()
+
+        d1, d2 = out.shape
+        selected_ids = np.random.choice(d2, size=s, replace=False)
+
+        return torch.tensor(out)[:,selected_ids]
+        #return torch.tensor(out)
 
 def load_data_syn(r=5, d1=5000, d2=200):
     mat1 = torch.randn(d1,r)
